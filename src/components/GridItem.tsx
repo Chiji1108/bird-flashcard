78cd5563c1fef5bfb93970fb6ad7b303d7588284
Chiji1108 <chiji@keio.jp>
2021-05-05 00:39:20 +0900

    🐛 replace aspect-ratio

--- src/components/GridItem.tsx
+++ src/components/GridItem.tsx
@@ -30,70 +30,72 @@ export default function GridItem({
   return (
     <li
       className={cn(
-        styles.square,
-        "relative bg-white rounded-lg overflow-hidden shadow flex items-center justify-center"
+        "aspect-w-1 aspect-h-1",
+        "relative bg-white rounded-lg overflow-hidden shadow"
       )}
     >
-      {isFlipped ? (
-        <a
-          href={url}
-          className="flex justify-center items-center space-x-2 group"
-        >
-          <p className="text-xl font-bold group-hover:underline">{name}</p>
-          <Icon className="text-gray-500 group-hover:text-gray-700">
-            open_in_new
-          </Icon>
-        </a>
-      ) : (
-        <Swiper
-          className="w-full h-full"
-          // navigation
-          scrollbar={{ draggable: true }}
-          preloadImages={false}
-          lazy
-          virtual
-        >
-          {images.map((image) => (
-            <SwiperSlide key={image}>
-              <img
-                loading="lazy"
-                src={image}
-                alt={name}
-                className="w-full h-full object-cover"
-              />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      )}
-      {audioIds.length === 0 || (
-        <>
-          <Modal open={open} onClose={() => setOpen(false)}>
-            <div className="flex flex-col space-y-4">
-              {audioIds.map((audioId) => (
-                <ReactPlayer
-                  key={audioId}
-                  url={`https://www.youtube.com/watch?v=${audioId}`}
+      <div className="flex items-center justify-center">
+        {isFlipped ? (
+          <a
+            href={url}
+            className="flex justify-center items-center space-x-2 group"
+          >
+            <p className="text-xl font-bold group-hover:underline">{name}</p>
+            <Icon className="text-gray-500 group-hover:text-gray-700">
+              open_in_new
+            </Icon>
+          </a>
+        ) : (
+          <Swiper
+            className="w-full h-full"
+            // navigation
+            scrollbar={{ draggable: true }}
+            preloadImages={false}
+            lazy
+            virtual
+          >
+            {images.map((image) => (
+              <SwiperSlide key={image}>
+                <img
+                  loading="lazy"
+                  src={image}
+                  alt={name}
+                  className="w-full h-full object-cover"
                 />
-              ))}
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
+        {audioIds.length === 0 || (
+          <>
+            <Modal open={open} onClose={() => setOpen(false)}>
+              <div className="flex flex-col space-y-4">
+                {audioIds.map((audioId) => (
+                  <ReactPlayer
+                    key={audioId}
+                    url={`https://www.youtube.com/watch?v=${audioId}`}
+                  />
+                ))}
+              </div>
+            </Modal>
+            <div className="absolute left-2 bottom-2 z-10">
+              <Button onClick={() => setOpen(true)}>
+                <Icon className="text-white">volume_up</Icon>
+              </Button>
             </div>
-          </Modal>
-          <div className="absolute left-2 bottom-2 z-10">
-            <Button onClick={() => setOpen(true)}>
-              <Icon className="text-white">volume_up</Icon>
-            </Button>
-          </div>
-        </>
-      )}
+          </>
+        )}
 
-      <div className="absolute right-2 bottom-2 z-10">
-        <Button onClick={() => setFlipped((prev) => !prev)}>
-          <Icon className="text-white">flip</Icon>
-        </Button>
-      </div>
-      <div className="absolute right-2 top-2 z-10">
-        <Button onClick={() => setDone(true)}>
-          <Icon className="text-white">done</Icon>
-        </Button>
+        <div className="absolute right-2 bottom-2 z-10">
+          <Button onClick={() => setFlipped((prev) => !prev)}>
+            <Icon className="text-white">flip</Icon>
+          </Button>
+        </div>
+        <div className="absolute right-2 top-2 z-10">
+          <Button onClick={() => setDone(true)}>
+            <Icon className="text-white">done</Icon>
+          </Button>
+        </div>
       </div>
     </li>
   );
